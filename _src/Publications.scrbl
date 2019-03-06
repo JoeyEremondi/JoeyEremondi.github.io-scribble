b117e5285ccae5d5bd39be0b76814560313e12cd
Joey Eremondi <joey.eremondi@gmail.com>
2019-03-05 20:38:49 -0800

    Macro to generate for pubs from particular file

--- _src/Publications.scrbl
+++ _src/Publications.scrbl
@@ -1,14 +1,5 @@
 #lang scribble/manual
 @require["bib.rkt"]
 
-Publications go here
+@(all-pubs)
  
-@(define (allcites) (for-each 
-                (lambda (x) (~cite x))
-                   mycites))
-
-@allcites{}
-
-
-
-@(generate-bib #:sec-title "")
--- _src/bib.rkt
+++ _src/bib.rkt
@@ -1,9 +1,32 @@
 
 #lang racket
+ (require (for-syntax racket/syntax))
 (require scriblib/autobib scriblib/bibtex)
-(provide ~cite citet generate-bib mycites)
+(provide all-pubs)
 (provide (all-from-out scriblib/bibtex scriblib/autobib))
 
-(define-bibtex-cite "_src/mypubs.bib" ~cite citet generate-bib)
-(define mycites (hash-keys (bibdb-raw (path->bibdb "_src/mypubs.bib"))))
-(displayln mycites)
+;(define (do-phony-cites cite-list)
+;   (for-each ~cite cite-list)) 
+
+(define-syntax (makecite stx)
+  (syntax-case stx () 
+  [(_ bib-file ~cite citet generate-bib sec-title)
+   (let [(mycites (generate-temporary 'make-phony))]
+     #`(begin
+         (define-bibtex-cite bib-file ~cite citet generate-bib)
+         (define #,mycites (hash-keys (bibdb-raw (path->bibdb "_src/mypubs.bib"))))
+         (list (for-each ~cite #,mycites) (generate-bib #:sec-title sec-title))
+         )
+     )
+   ]
+  
+  ))
+
+(define-syntax-rule (all-pubs)
+  (makecite "_src/mypubs.bib" ~cite citet define-bib "Journal Papers")
+  )
+
+
+(makecite "_src/mypubs.bib" ~cite citet define-bib "Journal Papers")
+;(define mycites (hash-keys (bibdb-raw (path->bibdb "_src/mypubs.bib"))))
+;(displayln mycites)
