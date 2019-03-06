cb4a8845ac483493e8d81eab7cb6719668cd9936
Joey Eremondi <joey.eremondi@gmail.com>
2019-03-05 22:37:34 -0800

    Get multiple bibfiles working

--- _src/bib.rkt
+++ _src/bib.rkt
@@ -2,7 +2,7 @@
 #lang racket
  (require (for-syntax racket/syntax))
 (require scriblib/autobib scriblib/bibtex)
-(provide all-pubs)
+(provide all-pubs makecite)
 (provide (all-from-out scriblib/bibtex scriblib/autobib))
 
 ;(define (do-phony-cites cite-list)
@@ -10,12 +10,16 @@
 
 (define-syntax (makecite stx)
   (syntax-case stx () 
-  [(_ bib-file ~cite citet generate-bib sec-title)
-   (let [(mycites (generate-temporary 'make-phony))]
+  [(_ bib-file  sec-title)
+   (let [(mycites (generate-temporary 'make-phony))
+         (~cite (generate-temporary '~cite))
+         (citet (generate-temporary 'citet))
+         (generate-bib (generate-temporary 'generate-bib))
+         ]
      #`(begin
-         (define-bibtex-cite bib-file ~cite citet generate-bib)
-         (define #,mycites (hash-keys (bibdb-raw (path->bibdb "_src/mypubs.bib"))))
-         (list (for-each ~cite #,mycites) (generate-bib #:sec-title sec-title))
+         (define-bibtex-cite bib-file #,~cite #,citet #,generate-bib)
+         (define #,mycites (hash-keys (bibdb-raw (path->bibdb bib-file))))
+         (list (for-each #,~cite #,mycites) (#,generate-bib #:sec-title sec-title))
          )
      )
    ]
@@ -23,10 +27,13 @@
   ))
 
 (define-syntax-rule (all-pubs)
-  (makecite "_src/mypubs.bib" ~cite citet define-bib "Journal Papers")
-  )
+  (begin
+  (makecite "_src/journal_papers.bib"  "Journal Papers")
+  (makecite "_src/conf_papers.bib"  "Conference Papers")
+  (makecite "_src/conf_versions.bib"  "Conference Versions of Journal Papers Papers")
+  
+  ))
 
 
-(makecite "_src/mypubs.bib" ~cite citet define-bib "Journal Papers")
 ;(define mycites (hash-keys (bibdb-raw (path->bibdb "_src/mypubs.bib"))))
 ;(displayln mycites)
